11e34abf4fb438e2e160aa517dfd0bb34472033a
EwyBoy <ewyboy@gmail.com>
2019-05-23 04:59:25 +0200

    Fixed IntelliJ & Removed Legacy Code

---
+++ src/Database/User.java
@@ -0,0 +1,14 @@
+package Database;
+
+import movie.Movie;
+
+import java.util.List;
+
+public class User {
+
+    public User(List<Movie> movies, List<String> genres) {
+
+    }
+
+
+}
---
+++ src/movie/MovieRecommender.java
@@ -0,0 +1,13 @@
+package movie;
+
+import Database.User;
+
+public class MovieRecommender {
+
+    public MovieRecommender() {}
+
+    public static void recommend(User user) {
+
+    }
+
+}
